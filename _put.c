ecd434e4729f285c5abb3f6badd33324c0c8085d
sihahi <siham.hida27@gmail.com>
2023-06-02 15:11:22 -0700

    task printf

--- _put.c
+++ _put.c
@@ -11,7 +11,7 @@ int _puts(char *str)
 	char *a = str;
 
 	while (*str)
-		_putchar (*str++);
+		_putchar(*str++);
 	return (str - a);
 }
 
@@ -24,7 +24,6 @@ int _puts(char *str)
 int _putchar(int c)
 {
 	static int i;
-
 	static char buf[OUTPUT_BUF_SIZE];
 
 	if (c == BUF_FLUSH || i  >= OUTPUT_BUF_SIZE)
